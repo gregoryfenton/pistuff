85c9cafe1e7d1b3241346a44f77aeb1e7145a626
gregoryfenton <greg@gregoryfenton.com>
2013-03-17 12:37:10 +0000

    revision 1.1

--- downloadmagazines.sh
+++ downloadmagazines.sh
@@ -5,7 +5,7 @@ a=~/magpi
 
 mkdir -p "$a"
 cd "$a"
-for n in seq 1 1 10; do
+for n in `seq 1 1 10`; do
     f="The-MagPi-issue-$n-en.pdf"
     if [ ! -f "$f" ]; then
         wget "http://www.themagpi.com/en/issue/$n/pdf/" -q -O "$f"
